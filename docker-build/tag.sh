31a01aaac7eed2f21de86fcf6ea63352eebbe9a5
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-05-18 20:16:12 +0800

    西元2024年05月18日 (週六) 20時16分12秒 CST

--- docker-build/tag.sh
+++ docker-build/tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node docker-build/update-citation.js
+node build/update-citation.js
 
 # =================================================================
 
@@ -25,9 +25,19 @@ GITHUB_USER=`echo $GITHUB_HOMEPAGE | awk -F'/' '{print $4}'`
 GITHUB_REPO=`jq -r '.name' package.json`
 #echo $GITHUB_REPO
 
+DOI=`jq -r '.doi' package.json`
+
 echo "================================================"
 echo "Integrate GitHub with Zenodo: https://rb.gy/ql60qi"
-echo "GitHub Homepage:  ${GITHUB_HOMEPAGE}"
-echo "Zenono GitHub Setting:  https://zenodo.org/account/settings/github/"
-echo "Zenono Repo:  https://zenodo.org/account/settings/github/repository/${GITHUB_USER}/${GITHUB_REPO}"
+echo ""
+echo "Steps:"
+echo "1. Zenono GitHub Setting:  https://zenodo.org/account/settings/github/"
+echo "2. GitHub New Release:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
+echo "3. Zenono Repository Management:  https://zenodo.org/account/settings/github/repository/${GITHUB_USER}/${GITHUB_REPO}"
+echo "4. Create new tag and push to GitHub."
+if [ -n "$DOI" ]; then
+  echo "5. GitHub New Release Again:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
+  echo "6. GitHub Homepage:  ${GITHUB_HOMEPAGE}"
+  echo "7. Zenono Public:  https://doi.org/${DOI}"
+fi
 echo "================================================"
