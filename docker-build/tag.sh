5a9544042d8a8ef64bc7c6a63b463ecc6de369ff
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-05-18 14:28:34 +0800

    20240518.142833

--- docker-build/tag.sh
+++ docker-build/tag.sh
@@ -4,3 +4,9 @@ new_version=$(date '+%Y%m%d.%H%M%S')
 
 git tag $new_version
 git push origin $new_version
+
+# =================================================================
+
+git add .
+git commit -m "${new_version}"
+git push --force-with-lease
